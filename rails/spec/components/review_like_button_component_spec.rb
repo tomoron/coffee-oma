c7490a5e320f01a70abc9f873c622fb1d831806d
wisdom-plus <tomoron326@outlook.jp>
2024-07-03 07:57:37 +0900

    add:review_like component

--- rails/spec/components/review_like_button_component_spec.rb
+++ rails/spec/components/review_like_button_component_spec.rb
@@ -3,13 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe ReviewLikeButton, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:review) { create(:review) }
+  let(:review_like) { create(:product_review_like, liked_id: review.id, user: user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  it 'renders the component(like)' do
+    render_inline(ReviewLikeButton::Component.new(review: review, review_like: review_like))
+
+    expect(page).to have_css('.destroy_button')
+  end
+
+  it 'renders the component(unlike)' do
+    render_inline(ReviewLikeButton::Component.new(review: review.decorate, review_like: nil))
+    expect(page).to have_css('.create_button')
+  end
 end
--- rails/spec/components/review_list_component_spec.rb
+++ rails/spec/components/review_list_component_spec.rb
@@ -3,13 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe ReviewList, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:review) { create(:review, user: user) }
+  let(:review_like) { create(:product_review_like, liked_id: review.id, user: user) }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
+  it 'renders a list of reviews' do
+    review_like
+    reviews = Review.show_review(review.product).page(1).per(5)
+    review_likes = user.where_review_likes(reviews, 'review')
+    render_inline(ReviewList::Component.new(reviews: reviews, review_likes: review_likes, current_user: user))
+
+    expect(page).to have_css('.review_item')
+  end
+
+  # it 'when not a review rendersa list of reviews' do
+  #   render_inline(ReviewList::Component.new(reviews: [], review_likes: nil, current_user: user))
+
+  #   expect(page).to have_text('このアイテムに寄せられたレビューはまだありません。')
   # end
 end
